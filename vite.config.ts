4aa0216911c2a8c14172945b2c7b0f0c58ad17c5
czheisenberg <czheisenberg@qq.com>
2026-06-23 21:53:22 +0800

    feat: 11

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,7 @@ import traeBadgePlugin from 'vite-plugin-trae-solo-badge'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // plugins: [vue()],
-  base: '/cats/'
+  base: '/cats/',
   server: {
     host: '0.0.0.0', // 允许局域网访问
     port: 5173,
